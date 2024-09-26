e1d05d164033e9528edde7c0fbdde51c321b6c20
Markimg22 <marquim.net.221200@hotmail.com>
2024-09-25 21:39:09 -0400

    refactor: organize structure of project

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div className="bg-black flex items-center justify-between p-10">
-            <h1 className="text-white animate-pulse">Teste</h1>
-            <p className="text-white animate-spin">Teste 2</p>
-        </div>
-    );
-}
